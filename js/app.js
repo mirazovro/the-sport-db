aa66dfa813d07bc5a363953f77cac7c35d77d537
unknown <mirazovro@gmail.com>
2022-09-01 11:39:01 +0600

    update-images

--- js/app.js
+++ js/app.js
@@ -1,44 +1,33 @@
 // alert('hello')
 
 const loadPlayer = () => {
-    const url = `https://www.thesportsdb.com/api/v1/json/2/searchevents.php?e=`
+    const url = `https://www.thesportsdb.com/api/v1/json/2/all_sports.php`
 
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPlayer(data.event));
+        .then(data => displayPlayer(data.sports));
 }
     
-const displayPlayer = (events) => {
-    console.log(events)
+const displayPlayer = (sports) => {
+    // console.log(sports)
 
     const playerContainer = document.getElementById('player-container')
-    events.forEach(event => {
-        // console.log(event)
+    sports.forEach(sport => {
+        console.log(sport)
 
         const newPlayer = document.createElement('div')
         newPlayer.classList.add('col')
         newPlayer.innerHTML = `
         
         <div class="card">
-      <img src="" class="card-img-top" alt="...">
+      <img src="${sport.strSportThumb}" class="card-img-top" alt="...">
       <div class="card-body">
-      <h2>${event.strLeague
+      <h2>${sport.strFormat
       } <h2>
-
-      <h4>${event.strLocked?event.strLocked:'This is locked man'}</h4>
-        <h5 class="card-title">${event.strEvent}</h5>
-        <h5 class="card-title">${event.dateEvent}</h5>
-        <h5 class="card-title">${event.strTime}</h5>
-        <h5 class="card-title">${event.strTimeLocal}</h5>
-        <h5 class="card-title">${event.strTimestamp
-        }</h5>
-        <h3>${event.strAwayTeam?event.strHomeTeam
-            :'No HOme team'
-        }</h3>
-        <h3>${event.strAwayTeam?event.strAwayTeam:'No Away team'
-        }</h3>
-        <h5>${event.strEventAlternate ? event.strEventAlternate : 'upcoming'}</h5>
-        <h2>${event.idEvent}</h2>
+      <h4>${sport.strSport?sport.strSport:'This is locked man'}</h4>
+        <h5 class="card-title">${sport.strSportDescription.slice(0,300)}</h5>
+        <img src="${sport.strSportIconGreen
+        }" class="card-img-top" alt="...">
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
         
